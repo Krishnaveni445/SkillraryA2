775da724f178cb76788b408a697a7fc4b12060cf
KRISHNAVENI <KRISHNAVENI@LAPTOP-JIII27DC>
2024-02-03 13:32:02 +0530

    Added Comment to AddCategoryTestJava

--- src/test/java/testScript/AddCategoryTest.java
+++ src/test/java/testScript/AddCategoryTest.java
@@ -7,7 +7,7 @@ import org.testng.asserts.SoftAssert;
 
 import genericUtilities.BaseClass;
 import genericUtilities.IConstantPath;
-
+//This test is used to add category to the application
 public class AddCategoryTest extends BaseClass {
 	@Test
 	public void addCategoryTest() {
